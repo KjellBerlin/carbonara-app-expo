15ce5104643979eb444ca83cd90110c813494d15
KjellBerlin <38405651+KjellBerlin@users.noreply.github.com>
2024-06-22 22:56:30 +0200

    Open in app browser

--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -7,21 +7,64 @@ import AddressCard from '../components/AddressCard';
 import { GlobalContext } from '../GlobalContext';
 import { Button } from '../components';
 import useCreateOrder from '../hooks/useCreateOrder';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 
 const { width } = Dimensions.get("screen");
 
 const OrderScreen = () => {
   const { state } = useContext(GlobalContext);
   const { fullName, product, address } = state;
-
   const [additionalDetailsFocus, setAdditionalDetailsFocus] = useState(false);
+  const { data, loading, createOrder } = useCreateOrder();
 
-  // Call the hook at the top level of the component
-  const { data, createOrder } = useCreateOrder();
+  const handlePress = async () => {
+    await createOrder();
+    if (!loading && data) {
+      const { orderId, paymentRedirectionLink } = data.createOrder;
+      console.log("Order: " + orderId + " Redirect link: " + paymentRedirectionLink);
+      if (paymentRedirectionLink) {
+        openInAppBrowser(paymentRedirectionLink);
+      }
+    }
+  };
 
-  const handlePress = () => {
-    createOrder();
-    console.log("Order: " + data.createOrder.orderId + " Redirect link: " + data.createOrder.paymentRedirectionLink);
+  const openInAppBrowser = async (url) => {
+    try {
+      if (await InAppBrowser.isAvailable()) {
+        const result = await InAppBrowser.open(url, {
+          // iOS Properties
+          dismissButtonStyle: 'cancel',
+          preferredControlTintColor: nowTheme.COLORS.PRIMARY,
+          readerMode: false,
+          animated: true,
+          modalPresentationStyle: 'pageSheet',
+          modalTransitionStyle: 'coverVertical',
+          modalEnabled: true,
+          enableBarCollapsing: false,
+          // Android Properties
+          showTitle: true,
+          toolbarColor: '#6200EE',
+          secondaryToolbarColor: 'black',
+          enableUrlBarHiding: true,
+          enableDefaultShare: true,
+          forceCloseOnRedirection: false,
+          animations: {
+            startEnter: 'slide_in_right',
+            startExit: 'slide_out_left',
+            endEnter: 'slide_in_left',
+            endExit: 'slide_out_right',
+          },
+          headers: {
+            'my-custom-header': 'my custom header value'
+          },
+        });
+        console.log(result);
+      } else {
+        Linking.openURL(url);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   const renderCards = () => {
