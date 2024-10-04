d5a663b91c51e13f3b1eba0ec88f4ea2dbe2dad6
KjellBerlin <38405651+KjellBerlin@users.noreply.github.com>
2024-10-04 15:21:07 +0200

    Use expo splash screen

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Image } from 'react-native';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
-import { Asset } from 'expo-asset';
 import { Block, GalioProvider } from 'galio-framework';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Screens from './navigation/Screens';
-import { Images, nowTheme } from './constants';
+import { nowTheme } from './constants';
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
 import { Auth0Provider } from 'react-native-auth0';
 import { setContext } from '@apollo/client/link/context';
 import * as SecureStore from 'expo-secure-store';
 
 import { GlobalProvider } from './GlobalContext';
-
-// cache app images
-const assetImages = [
-  Images.RegisterBackground
-];
 
 const httpLink = createHttpLink({
   uri: 'https://carbonara-core-mkvkriomda-ew.a.run.app/graphql',
@@ -39,15 +32,6 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-function cacheImages(images) {
-  return images.map(image => {
-    if (typeof image === 'string') {
-      return Image.prefetch(image);
-    } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
-}
 
 export default class App extends React.Component {
   state = {
@@ -55,53 +39,51 @@ export default class App extends React.Component {
     fontLoaded: false
   };
 
+  componentDidMount() {
+    SplashScreen.preventAutoHideAsync();
+    this._loadResourcesAsync();
+  }
+
   render() {
     if (!this.state.isLoadingComplete) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <Auth0Provider domain={"dev-yntwqm72gdl58ssy.us.auth0.com"} clientId={"df43s61p15MI3pp7UoBPV0tEQ0DA6dIc"}>
-          <ApolloProvider client={client}>
-            <GlobalProvider>
-              <NavigationContainer>
-                <GalioProvider theme={nowTheme}>
-                  <Block flex>
-                    <Screens />
-                  </Block>
-                </GalioProvider>
-              </NavigationContainer>
-            </GlobalProvider>
-          </ApolloProvider>
-        </Auth0Provider>
-      );
+      return null; // Keep the splash screen visible while resources are loading
     }
+
+    return (
+      <Auth0Provider domain={"dev-yntwqm72gdl58ssy.us.auth0.com"} clientId={"df43s61p15MI3pp7UoBPV0tEQ0DA6dIc"}>
+        <ApolloProvider client={client}>
+          <GlobalProvider>
+            <NavigationContainer>
+              <GalioProvider theme={nowTheme}>
+                <Block flex>
+                  <Screens />
+                </Block>
+              </GalioProvider>
+            </NavigationContainer>
+          </GlobalProvider>
+        </ApolloProvider>
+      </Auth0Provider>
+    );
   }
 
   _loadResourcesAsync = async () => {
-    await Font.loadAsync({
-      'montserrat-regular': require('./assets/font/Montserrat-Regular.ttf'),
-      'montserrat-bold': require('./assets/font/Montserrat-Bold.ttf'),
-      'next-sphere-black': require('./assets/font/Next-Sphere-Black.ttf'),
-      'next-sphere-thin': require('./assets/font/Next-Sphere-Thin.ttf'),
-    });
+    try {
+      await Font.loadAsync({
+        'montserrat-regular': require('./assets/font/Montserrat-Regular.ttf'),
+        'montserrat-bold': require('./assets/font/Montserrat-Bold.ttf'),
+        'next-sphere-black': require('./assets/font/Next-Sphere-Black.ttf'),
+        'next-sphere-thin': require('./assets/font/Next-Sphere-Thin.ttf'),
+      });
 
-    this.setState({ fontLoaded: true });
-    return Promise.all([...cacheImages(assetImages)]);
+      this.setState({ isLoadingComplete: true }, async () => {
+        await SplashScreen.hideAsync();
+      });
+    } catch (error) {
+      this._handleLoadingError(error);
+    }
   };
 
   _handleLoadingError = error => {
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    if (this.state.fontLoaded) {
-      this.setState({ isLoadingComplete: true });
-    }
+    console.warn("Loading error: ", error);
   };
 }
